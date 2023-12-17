6d370c6fa93c7ba48ca6c0738e30f01ecbf01d7d
ciach <ci3l4k@gmail.com>
2023-12-17 18:34:29 +0100

    improved core function

--- core_func.py
+++ core_func.py
@@ -3,7 +3,7 @@ def find_paths(node, my_dict, path, my_array, steps_list):
     word = [my_array[item] for item in path]
     if word.count("#") == 1:
         steps_list.append(path.copy())
-    if len(path) < 10:
+    if len(path) < 12:  # words longer than 12 letters are not allowed for CPU
         for next_node in my_dict[node]:
             if next_node not in path:
                 find_paths(next_node, my_dict, path, my_array, steps_list)
